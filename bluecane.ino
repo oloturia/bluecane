de0650d92c6faf81952792f913fb8f7fa6ca8cd4
oloturia <oloturia@gmail.com>
2015-03-05 22:33:24 +0100

    corrected wordwrap

--- bluecane.ino
+++ bluecane.ino
@@ -25,14 +25,12 @@ void loop() {
 }
 
 void recgps() {
-  String gpsDecode = ""; //every single char of the GPS is stored in
-this string, when it senses that a string is starting then
+  String gpsDecode = ""; //every single char of the GPS is stored in this string, when it senses that a string is starting then
   String latitude = "";  //stores the chars in the right variable.
   String longitude = "";
   char gpsChar;          //the single char from the GPS module
   boolean dollar = false;//every new line from GPS starts with a dollar
-  boolean gpgll = false; //the GPS outputs a lot of info, but we need
-just long and lat, $GPGLL line is the shortest
+  boolean gpgll = false; //the GPS outputs a lot of info, but we need just long and lat, $GPGLL line is the shortest
   int xind;              //an index
   while(gpsSerial.available()){
       gpsChar = gpsSerial.read();
@@ -52,34 +50,29 @@ just long and lat, $GPGLL line is the shortest
         }
       }
       if (gpgll) {
-        if ((xind >=2) && (xind <= 11)) { //latitude chars, from 2 to
-11 ("$GPGLL" excluded)
+        if ((xind >=2) && (xind <= 11)) { //latitude chars, from 2 to 11 ("$GPGLL" excluded)
           latitude.concat(gpsChar);
           if (xind == 3) {
             latitude.concat(*" ");
           }
         }
         if (xind == 13) {
-          if (gpsChar == *"S") {          //if in the southern
-hemisphere, puts the minus sign
+          if (gpsChar == *"S") {          //if in the southern hemisphere, puts the minus sign
             latitude = "-"+latitude;
           }
         }
-        if ((xind >=15) && (xind <=25)) { //longitude stays between
-the 15th and the 25th char
+        if ((xind >=15) && (xind <=25)) { //longitude stays between the 15th and the 25th char
           longitude.concat(gpsChar);
           if (xind == 17) {
             longitude.concat(*" ");
           }
         }
         if (xind == 27) {
-            if (gpsChar == *"W") {        //it's negative in the
-western hemisphere
+            if (gpsChar == *"W") {        //it's negative in the western hemisphere
               longitude = "-"+longitude;
             }
         }
-        if (xind == 28) {                //28 there are more things in
-the GPS' output, like clock and crc, but they are no use
+        if (xind == 28) {                //28 there are more things in the GPS' output, like clock and crc, but they are no use
           Serial.print(latitude);       //for our purpose, at the moment
           Serial.print(",");
           Serial.println(longitude);
